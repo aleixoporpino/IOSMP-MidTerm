1154e4d00a1edc66b7134f980afc15c5369eb5cf
Jose Aleixo Araujo Porpino Filho <aleixoporpino@gmail.com>
2018-10-18 20:19:23 -0400

    Added favorite button with control

--- Simple Shopping List/Simple Shopping List/ViewController.swift
+++ Simple Shopping List/Simple Shopping List/ViewController.swift
@@ -23,7 +23,24 @@ class ViewController: UIViewController {
         lbl1.text = "Qty: \(Int(stepper1.value))"
     }
     
+    @IBAction func onClickFav(_ sender: UIButton) {
+        if sender.currentTitle! ==  "★" {
+            sender.setTitle("☆", for: .normal)
+            sender.setTitleColor(UIColorFromRGB(0x1E6FAF), for: .normal)
+        } else {
+            sender.setTitle("★", for: .normal)
+            sender.setTitleColor(UIColorFromRGB(0xFFD300), for: .normal)
+        }
+    }
     
+    func UIColorFromRGB(_ rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
     
 }
 
